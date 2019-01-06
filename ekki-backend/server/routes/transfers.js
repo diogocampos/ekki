df90fd33b1f52975e23ac569b84624ee0a78ad69
Diogo Campos <diogocampos@users.noreply.github.com>
2019-01-06 18:44:14 -0200

    Add transfer within the balance

--- ekki-backend/server/routes/transfers.js
+++ ekki-backend/server/routes/transfers.js
@@ -3,8 +3,39 @@ const express = require('express')
 const { authenticate } = require('./users')
 const Transfer = require('../db/Transfer')
 const { wrap } = require('../middleware')
+const User = require('../db/User')
 
 const router = express.Router()
+
+/**
+ * Makes a transfer to another user.
+ */
+router.post('/', authenticate, [
+  wrap(async (req, res) => {
+    const { to, amount } = req.body.transfer
+    const sender = res.locals.user
+
+    const receiver = await User.findOne({ username: to })
+    if (!receiver) return
+
+    const [senderBalance, receiverBalance] = await Promise.all([
+      Transfer.getBalanceForUsername(sender.username),
+      Transfer.getBalanceForUsername(receiver.username),
+    ])
+
+    const data = {
+      sender: sender.username,
+      receiver: receiver.username,
+      amountFromBalance: amount,
+      amountFromCard: 0,
+      senderBalance: senderBalance - amount,
+      receiverBalance: receiverBalance + amount,
+    }
+
+    const transfer = await new Transfer(data).save()
+    res.json({ transfer })
+  }),
+])
 
 /**
  * Returns the user's history of sent and received transfers.
--- ekki-backend/server/tests/transfers.test.js
+++ ekki-backend/server/tests/transfers.test.js
@@ -6,7 +6,67 @@ const { requiresAuthentication } = require('./helpers')
 const app = require('../app')
 const Transfer = require('../db/Transfer')
 
+const { any } = expect
 const { authenticated } = fixtures
+
+describe('POST /transfers', () => {
+  const req = authenticated(body =>
+    request(app)
+      .post('/transfers')
+      .send(body)
+  )
+
+  requiresAuthentication(req)
+
+  describe('with valid data', () => {
+    const sender = authenticated.user
+    const receiver = fixtures.contactsOf(sender)[0]
+    let amount
+
+    specify('with enough balance', async () => {
+      amount = 1
+      const transfer = { to: receiver.username, amount }
+      const res = await req({ transfer }).expect(200)
+
+      const expected = {
+        sender: sender.username,
+        receiver: receiver.username,
+        amountFromBalance: amount,
+        amountFromCard: 0,
+      }
+
+      // stores the transfer in the database
+      const transferDoc = await Transfer.findById(res.body.transfer._id)
+      expect(transferDoc).toMatchObject({
+        ...expected,
+        senderBalance: any(Number),
+        receiverBalance: any(Number),
+        createdAt: any(Date),
+      })
+
+      // includes info in response body
+      expect(res.body).toEqual({
+        transfer: {
+          ...expected,
+          createdAt: transferDoc.createdAt.getTime(),
+          _id: transferDoc._id.toHexString(),
+        },
+      })
+    })
+
+    afterEach("updates the sender's balance", async () => {
+      const oldBalance = fixtures.balanceOf(sender)
+      const newBalance = await Transfer.getBalanceForUsername(sender.username)
+      expect(newBalance).toEqual(oldBalance - amount)
+    })
+
+    afterEach("updates the receiver's balance", async () => {
+      const oldBalance = fixtures.balanceOf(receiver)
+      const newBalance = await Transfer.getBalanceForUsername(receiver.username)
+      expect(newBalance).toEqual(oldBalance + amount)
+    })
+  })
+})
 
 describe('GET /transfers', () => {
   const req = authenticated(() => request(app).get('/transfers'))
